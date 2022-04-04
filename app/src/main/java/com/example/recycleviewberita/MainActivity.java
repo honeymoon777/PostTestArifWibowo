a55855eb88886c9114ad7f6f79ea81d36f0c2fe7
nip <roughchaser@gmail.com>
2022-04-04 14:21:16 +0700

    fix error image with glide

--- app/src/main/java/com/example/recycleviewberita/MainActivity.java
+++ app/src/main/java/com/example/recycleviewberita/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         rvBerita=findViewById(R.id.rvBerita);
         listBerita.add(new BeritaModel("Orang ini mati 3 kali, ini kata resti","Health","https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2022/03/30/2810251709.jpg"));
-        listBerita.add(new BeritaModel("Juara Coding itu mantap, ini kata nining","Lifestyle","https://lms.juaracoding.com/pluginfile.php/1/theme_lambda/slide1image/1648385307/foto0.jpg"));
+        listBerita.add(new BeritaModel("Juara Coding itu mantap, ini kata nining","Lifestyle","https://juaracoding.com/public/dummy/img_pak_paulus.png"));
         listBerita.add(new BeritaModel("Banci ini ditangkap warga karna tidak banci","Travel","https://cdn0-production-images-kly.akamaized.net/C78TuqRI6EvujNd5nHZApBQ9apw=/1200x900/smart/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/3951801/original/052272700_1646356554-Tessy_1.jpg"));
 
         BeritaAdapter adapter = new BeritaAdapter(listBerita,this);
